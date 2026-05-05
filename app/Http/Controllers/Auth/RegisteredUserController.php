5e7a1bcfee81e25f3a671299a0a64953d51715eb
jeanhrm <ingempjordonez@gmail.com>
2026-05-05 13:11:28 -0500

    fix redirect dashboard Laravel 13

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -59,7 +59,7 @@ class RegisteredUserController extends Controller
             event(new Registered($user));
             Auth::login($user);
 
-            return redirect(RouteServiceProvider::HOME);
+            return redirect()->route('dashboard');
         }
     
 }
