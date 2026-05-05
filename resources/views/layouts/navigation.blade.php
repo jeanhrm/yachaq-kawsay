a5e983498e4a1a7d3c1420a471e9af58b33d4a84
jeanhrm <ingempjordonez@gmail.com>
2026-05-05 13:15:41 -0500

    fix navigation profile route

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -34,8 +34,8 @@
                     </x-slot>
 
                     <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
+                        <x-dropdown-link :href="#">
+                            {{ __('Mi perfil') }}
                         </x-dropdown-link>
 
                         <!-- Authentication -->
