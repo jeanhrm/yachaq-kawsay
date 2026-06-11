cb5506a0c34c56656d0dd4fca66dbad55dff764d
jeanhrm <ingempjordonez@gmail.com>
2026-06-10 19:42:22 -0500

    favicon tupac todos los layouts

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="{{ asset('images/tupac.png') }}">
     <title>Yachaq Kawsay — Aprende indagando</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
